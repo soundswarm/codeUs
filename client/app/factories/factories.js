3e77a10a5fc16f01cf95c67f128de8155bea9c7a
Hack Reactor Students <accounts@hackreactor.com>
2015-06-18 17:47:35 -0700

    Project Structure

---
+++ client/app/factories/factories.js
@@ -0,0 +1,52 @@
+angular.module('GitUs.factories', [])
+
+//
+
+
+.factory('Auth', function ($http, $location, $window) {
+  // Don't touch this Auth service!!!
+  // it is responsible for authenticating our user
+  // by exchanging the user's username and password
+  // for a JWT from the server
+  // that JWT is then stored in localStorage as 'com.shortly'
+  // after you signin/signup open devtools, click resources,
+  // then localStorage and you'll see your token from the server
+  var signin = function (user) {
+    return $http({
+      method: 'POST',
+      url: '/haiku/users/signin',
+      data: user
+    })
+    .then(function (resp) {
+      return resp.data.token;
+    });
+  };
+
+  var signup = function (user) {
+    return $http({
+      method: 'POST',
+      url: '/haiku/users/signup',
+      data: user
+    })
+    .then(function (resp) {
+      return resp.data.token;
+    });
+  };
+
+  var isAuth = function () {
+    return !!$window.localStorage.getItem('com.shortly');
+  };
+
+  var signout = function () {
+    $window.localStorage.removeItem('com.shortly');
+    $location.path('/signin');
+  };
+
+
+  return {
+    signin: signin,
+    signup: signup,
+    isAuth: isAuth,
+    signout: signout
+  };
+});
---
+++ server/server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+
+var app = express();
+
+require('./config/middleware.js')(app, express);
+
+module.exports = app;
+
+app.listen(8000);
