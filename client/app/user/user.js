d722bcf55ee2e2a88c1827c84d5a256a6540ab0a
Sean Connor <seantconnor@gmail.com>
2015-06-22 18:13:54 -0700

    added client folder with user and signin view

---
+++ client/app/user/user.js
@@ -0,0 +1,17 @@
+var mod = angular.module('GitUs.user', [])
+mod.controller('UserController', function($scope, $http) {
+  $scope.user = {};
+  $scope.user.languages = { 
+    ObjectCoffeeScript: 458, 
+    JavaScript: 1234,
+    Ruby: 23810
+  };
+
+  $http.get('https://api.github.com/users/soundswarm')
+    .success(function(data) {
+      $scope.user.login = data.login;
+      $scope.user.avatar_url = data.avatar_url;
+      $scope.user.url = data.html_url;
+
+    });
+})
