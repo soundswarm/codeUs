42e366f675faa3d4f2c9f48bbf86ec74bf1f8886
Sean Connor <seantconnor@gmail.com>
2015-06-24 14:54:46 -0700

    changed scores filed names in db. added scores functions that work. need to add token

--- server/app/helpers/helpers.js
+++ server/app/helpers/helpers.js
@@ -1,71 +1,159 @@
 // populate a single Coder's profile 
 var Coder = require('../models/coder');
 var Coders = require('../collections/coders');
-// var Promise = require('bluebird');
-var apicalls = require('request-promise');
+var rp = require('request-promise');
+var bb = require('bluebird');
+var _ = require('underscore');
+// var token =;//add token
 
+
+var api = {
+//get user
+//get repos
+// for each repos
+  // extend languages
+  // extend cred
+  // extend technologies
+// total repoScores
+  root:'https://api.github.com',
+  options: {
+    headers: {
+      'User-Agent': 'CodeUs-App',
+      'Authorization': 'token '+token 
+    },
+    transform: function(body, response) {  
+      return JSON.parse(body);
+    }
+  },
+  authenticateUser: function(token) {
+    return user;
+  },
+  getUser: function(username) {
+    this.options.url = this.root+'/users/'+username;
+    return rp(this.options);
+    // returns user object
+  },
+  getRepos: function(user) {
+    this.options.url  = user.repos_url;
+    return rp(this.options);
+    //returns array of repo objects
+  },
+  getReposLanguages: function(repos) {
+    return bb.all(repos.map(function(repo) {
+      this.options.url = repo.languages_url;
+      return rp(this.options)
+     }, this))
+     .then(function(languages) {
+        for(var i=0;i<repos.length; i++) {
+          repos[i].languages= languages[i];
+        }
+        return repos;
+      })
+  },
+  getReposCred: function(repos) {
+
+  },
+  getReposTechnologies: function(repos) {
+
+  },
+  reposScores: function(repos) {
+    var scores = {};
+    _.each(repos, function(repo) {
+      
+      //calculate languages
+      _.each(repo.languages, function(kilobytes, language) {
+        if(scores[language]) {
+          scores[language] += Math.round(kilobytes/1000);
+        } else {
+          scores[language] = Math.round(kilobytes/1000);
+        }
+      })
+
+      //calculate stargazers
+      if(scores.stargazers_count) {
+        scores.stargazers_count += repo.stargazers_count
+      } else {
+        scores.stargazers_count = repo.stargazers_count
+      }
+
+      //calculate watchers
+      if(scores.watchers_count) {
+        scores.watchers_count += repo.watchers_count
+      } else {
+        scores.watchers_count = repo.watchers_count
+      }
+      //calculate forks
+      if(scores.forks) {
+        scores.forks += repo.forks
+      } else {
+        scores.forks = repo.forks
+      }
+    })
+    return scores;
+  },
+  saveTodB: function(username, scores) {
+    new Coder({gh_username: username})
+    .fetch()
+    .then(function(coder) {
+      // console.log(scores.stargazers_count);
+      coder.save({
+        stargazers_count: scores.stargazers_count,
+        watchers_count: scores.watchers_count,
+        forks: scores.forks
+      })
+      .then(function(coder) {
+        Coders.set(coder)
+      })
+      // console.log(coder);
+    })
+  }
+  
+};
+var username = 'soundswarm';
+
+//create a new coder. DELETE THIS. JUST HERE FOR TESTING
 // var coder = new Coder({
-// 	gh_username: 'dummytesting',
-// 	name: 'Testing Dummy'
+//  gh_username: username,
 // });
-
 // coder.save().then(function(newCoder){
-// 	Coders.add(newCoder);
-// 	console.log('new coder added to collection', newCoder);
+//  Coders.add(newCoder);
+//  console.log('new coder added to collection', newCoder);
 // });
 
-var options = {
-	url: 'https://api.github.com/users?access_token=2045c650b980c19a21fba62b62b81f0cf8ef6853',
-	headers: {
-		'User-Agent': 'CodeUs-App'
-	}
-};
+//get repos scores then add them to database
+api.getUser(username).promise().bind(api)
+  .then(api.getRepos)
+  .then(api.getReposLanguages)
+  .then(api.reposScores)
+  .then(function(scores) {
+    api.saveTodB(username, scores);
+  })
+  .then(function() {
 
-apicalls(options)
-	.then(function(response) {
-		var token = '2045c650b980c19a21fba62b62b81f0cf8ef6853';
-		for (var i=0; i < 1; i++) {
-			var parsed = (JSON.parse(response))[i];
-			options.url = 'https://api.github.com/users/' + parsed.login + '?access_token=' + token;
-			console.log(options.url);
-			setInterval(addCoder(options), 500);
-		}
-	});
-
-var addCoder = function(options) {
-	apicalls(options)
-	.then(function(response) {
-		var parsed = JSON.parse(response);
-		var coder = new Coder({
-			gh_username: parsed.login,
-			name: parsed.name,
-			location: parsed.location,
-			email: parsed.email,
-			gh_site_url: parsed.blog,
-			photo_url: parsed.avatar_url,
-			gh_member_since: parsed.created_at
-		});
-		coder.save().then(function(newCoder){
-			Coders.add(newCoder);
-		});
-	});
-};
-
-module.exports = {
-
-	token: '2045c650b980c19a21fba62b62b81f0cf8ef6853',
-
-	initUser: function(token) {
-		return user;
-	}
+  })
+  .catch(console.error)
+  // .then(function(b){
+  //   console.dir(b)
+  // })
 
 
 
 
-
-
-
-
-
-};
+// rp(options)
+//  .then(function(response) {
+//    var parsed = JSON.parse(response);
+    // var coder = new Coder({
+    //  gh_username: parsed.login,
+    //  name: parsed.name,
+    //  location: parsed.location,
+    //  email: parsed.email,
+    //  gh_site_url: parsed.blog,
+    //  photo_url: parsed.avatar_url,
+    //  gh_member_since: parsed.created_at
+    // });
+    // coder.save().then(function(newCoder){
+    //  Coders.add(newCoder);
+    //  console.log('new coder added to collection', newCoder);
+    // });
+// });
 
