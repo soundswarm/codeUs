1f07e5f4dbdffb6107c2555feee67a51fc89f898
Jason Menayan <jasonmenayan@gmail.com>
2015-06-24 14:57:07 -0700

    trying to get routes configured

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -8,6 +8,7 @@ var morgan      = require('morgan'), // used for logging incoming request
 module.exports = function (app, express) {
   // Express 4 allows us to use multiple routers with their own configurations
   var oauthRouter = express.Router();
+  var apiRouter = require('../routes/api.js');
 
 
   app.use(morgan('dev'));
@@ -17,6 +18,7 @@ module.exports = function (app, express) {
 
 
   app.use(oauthRouter); // use user router for all user request
+  app.use('/api', apiRouter);
 
   app.use(helpers.errorLogger);
   app.use(helpers.errorHandler);
@@ -25,5 +27,7 @@ module.exports = function (app, express) {
 
   // inject our routers into their respective route file
   require('../users/oauthRoutes.js')(oauthRouter);
+  apiRouter(app);
+
 
 };
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,61 +1,66 @@
 // add this to server.js:
 // app.use('/api', apiRouter);
 
-var apiRouter = express.Router();
+// var apiRouter = express.Router();
 var helper = require('../app/helpers/helpers');
 var api = require('request-promise');
-var Coder = require('../models/coder');
-var Coders = require('../collections/coders');
+var Coder = require('../app/models/coder');
+var Coders = require('../app/collections/coders');
+var router = require('express').Router();
 
-apiRouter.get('/populate', function() {
-	// ***** PSEUDOCODE *******
-	// ((this endpoint should be hit using some sort of cron))
-	// some sort of setInterval with looping across the helper functions (using 'since' parameter)
-	setInterval(function() {
-		helper.apicalls();
-	}, 1000);
-});
+module.exports = function () {
 
-// should be called with the following endpoint syntax: GET /api/realtime?u={username}
-apiRouter.get('/realtime', function(req, res, next) {
-	var token = "passed in"; // do not upload to GitHub with this token assigned explicitly!
-	var options = {
-		url: 'https://api.github.com/users/',
-		headers: {
-			'User-Agent': 'CodeUs-App'
-		};
-	};
-	options.url += req.query.u + '?access_token=' + token;
-	// fetch real-time user attr from API, assign to new Coder instance
-	api(options)
-	.then(function(response) {
-		var parsed = JSON.parse(response);
-		var coder = new Coder({
-			followers: parsed.followers,
-			updated_at: parsed.updated_at,
-			repo_count: parsed.public_repos
-		});
-	})
-	// fetch rest of the data from the database
-	.then(function() {
-		new Coder({'gh_username': req.query.u}).fetch();
-	})
-	// assign DB attr to the current Coder model instance object
-	.then(function(userModel) {
-		if (!userModel) {
-			console.log('User model ' + req.query.u + 'not found');
-		} else {
-			coder.gh_username = req.query.u;
-			coder.name = userModel.name;
-			coder.location = userModel.location;
-			coder.email = userModel.email;
-			coder.gh_site_url = userModel.blog;
-			coder.photo_url = userModel.avatar_url;
-			coder.gh_member_since = userModel.created_at;
-		}
+	router.get('/populate', function() {
+		// ***** PSEUDOCODE *******
+		// ((this endpoint should be hit using some sort of cron))
+		// some sort of setInterval with looping across the helper functions (using 'since' parameter)
+		setInterval(function() {
+			helper.apicalls();
+		}, 1000);
 	});
-	res.status(200).send(coder);
-});
 
+	// should be called with the following endpoint syntax: GET /api/realtime?u={username}
+	router.get('/realtime', function(req, res, next) {
+		console.log('/realtime route hit');
+		var token = "fb06bcad775c4e3fc9b99fa7271b56ba3a8e6425"; // do not upload to GitHub with this token assigned explicitly!
+		var options = {
+			url: 'https://api.github.com/users/',
+			headers: {
+				'User-Agent': 'CodeUs-App'
+			}
+		};
+		options.url += req.query.u + '?access_token=' + token;
+		// fetch real-time user attr from API, assign to new Coder instance
+		api(options)
+		.then(function(response) {
+			var parsed = JSON.parse(response);
+			var coder = new Coder({
+				followers: parsed.followers,
+				updated_at: parsed.updated_at,
+				repo_count: parsed.public_repos
+			});
+		})
+		// fetch rest of the data from the database
+		.then(function() {
+			new Coder({'gh_username': req.query.u}).fetch();
+		})
+		// assign DB attr to the current Coder model instance object
+		.then(function(userModel) {
+			if (!userModel) {
+				console.log('User model ' + req.query.u + 'not found');
+			} else {
+				coder.gh_username = req.query.u;
+				coder.name = userModel.name;
+				coder.location = userModel.location;
+				coder.email = userModel.email;
+				coder.gh_site_url = userModel.blog;
+				coder.photo_url = userModel.avatar_url;
+				coder.gh_member_since = userModel.created_at;
+			}
+		});
+		console.log(coder);
+		res.status(200).send(coder);
+	});
 
-module.exports = apiRouter;
+	return router;
+};
