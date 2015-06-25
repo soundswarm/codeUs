eec6b0331dbae7aca08ea390fcc4555a222ef8a5
Jason Menayan <jasonmenayan@gmail.com>
2015-06-25 15:30:25 -0700

    implement /addsodata to populate DB records with stackoverflow data

--- server/routes/api.js
+++ server/routes/api.js
@@ -44,9 +44,60 @@ module.exports = function (app) {
 					coder.gh_site_url = userModel.attributes.blog;
 					coder.photo_url = userModel.attributes.avatar_url;
 					coder.gh_member_since = userModel.attributes.created_at;
+					coder.so_reputation = userModel.attributes.so_reputation;
+					coder.so_answer_count = userModel.attributes.so_answer_count;
+					coder.so_question_count = userModel.attributes.so_question_count;
+					coder.so_upvote_count = userModel.attributes.so_upvote_count;
 					res.status(200).send(coder);
 				}
 			});
 		})
 	});
+
+	app.get('/addsodata', function(req, res, next) {
+		// console.log('/addsodata route hit');
+		var pageNumber = 1;
+		var hasMore = true;
+		var quotaRemain = 10;
+		var options = {
+			url: 'https://api.stackexchange.com/2.2/users?pagesize=100&order=desc&sort=reputation&site=stackoverflow&filter=!Ln4IB)_.hsRjrBGzKe*i*W&page=' + pageNumber,
+			gzip: true
+		};
+		// while (quotaRemain > 5 && hasMore === true) {
+			// console.log('/addsodata while loop entered');
+			// console.log('link', options.url);
+			api(options)
+			.then(function(response) {
+				// console.log('response', response);
+				var parsed = JSON.parse(response);
+				// console.log('parsed', parsed);
+				pageNumber++;
+				hasMore = parsed.has_more;
+				// console.log('parsed.has_more', parsed.has_more);
+				quotaRemain = parsed.quota_remaining;
+				// console.log('parsed.quota_remaining', parsed.quota_remaining);
+				parsed.items.forEach(function(user, i, items) {
+					// console.log('index', i, 'user', user);
+					new Coder({'name': user.display_name}).fetch()
+					.then(function(userModel) {
+						if (!userModel) {
+							// console.log('No user named ' + user.display_name + ' among GH users in DB.');
+						} else {
+							userModel.save({
+								so_location: user.location,
+								so_name: user.display_name,
+      					so_member_since: user.creation_date,
+      					so_reputation: user.reputation,
+      					so_answer_count: user.answer_count,
+      					so_question_count: user.question_count,
+      					so_upvote_count: user.up_vote_count,
+      					so_site_url: user.website_url
+							});
+						}
+					});
+				});
+			});
+		// }
+	});
+
 };
