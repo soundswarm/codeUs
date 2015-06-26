16085d53602b9f8ae6c0d2fa16b94b66ea968c65
Jason Menayan <jasonmenayan@gmail.com>
2015-06-26 11:16:17 -0700

    further work on /populate route in routes/api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -13,15 +13,16 @@ module.exports = function (app) {
 	app.get('/realtime', function(req, res, next) {
 		console.log('/realtime route hit');
 		var coder = {};
-		var token = "fb06bcad775c4e3fc9b99fa7271b56ba3a8e6425"; // do not upload to GitHub with this token assigned explicitly!
+		var token = "<get from Evernote>"; // do not upload to GitHub with this token assigned explicitly!
 		var options = {
 			url: 'https://api.github.com/users/',
 			headers: {
-				'User-Agent': 'CodeUs-App'
+				'User-Agent': 'CodeUs-App',
+				'Authorization': 'token '+ token 
 			}
 		};
-		options.url += req.query.u + '?access_token=' + token;
-		// fetch real-time user attr from API, assign to new Coder instance
+		options.url += req.query.u;
+		// fetch real-time user attr from API, assign to empty coder object
 		api(options)
 		.then(function(response) {
 			var parsed = JSON.parse(response);
@@ -51,7 +52,7 @@ module.exports = function (app) {
 					res.status(200).send(coder);
 				}
 			});
-		})
+		});
 	});
 
 	app.get('/addsodata', function(req, res, next) {
@@ -100,4 +101,65 @@ module.exports = function (app) {
 		// }
 	});
 
+	app.get('/populate', function(req, res, next) {
+		console.log('/populate route hit');
+		var token = "88a060383b6649c35278dd71db147f8a05cc8ea1"; // do not upload to GitHub with this token assigned explicitly!
+		var since = 0;
+		var full = true;
+		var options = {
+			url: 'https://api.github.com/users?per_page=100',
+			headers: {
+				'User-Agent': 'CodeUs-App',
+				'Authorization': 'token '+ token 
+			}
+		};
+		console.log('full: ', full);
+		// fetch users from GET /users call, save to db
+		while (full == true) {
+			console.log('while loop entered');
+			console.log('options: ', options);
+			api(options)
+			.then(function(response) {
+				var parsed = JSON.parse(response);
+				console.log('full parsed', parsed);
+				if (parsed.length < 100) { full = false;}
+				for (var i=0; i < parsed.length; i++) {
+					since = parsed[i].id;
+					console.log('record ' + since + ': ' + parsed[i]);
+					new Coder({gh_username: parsed.login}).fetch()
+	    			.then(function(coder) {
+	    				if (coder) {
+	    					console.log('existing coder: ', coder.gh_username);
+				      	coder.save({
+				      		gh_username: parsed[i].login,
+									name: parsed[i].name,
+									location: parsed[i].location,
+									email: parsed[i].email,
+									gh_site_url: parsed[i].blog,
+									photo_url: parsed[i].avatar_url,
+									gh_member_since: parsed[i].created_at
+				      	});
+	    				} else {
+	    					var newCoder = new Coder({
+				      		gh_username: parsed[i].login,
+									name: parsed[i].name,
+									location: parsed[i].location,
+									email: parsed[i].email,
+									gh_site_url: parsed[i].blog,
+									photo_url: parsed[i].avatar_url,
+									gh_member_since: parsed[i].created_at	    						
+	    					});
+	    					newCoder.save()
+	    						.then(function(coder) {
+        						Coders.set(coder);
+      						});
+			      	console.log('Coder ' + parsed[i].name + ' added to DB');
+						}
+					});
+			options.url = 'https://api.github.com/users?per_page=100&since=' + since;
+			}
+		})
+		.catch(console.error);
+   }
+	});
 };
